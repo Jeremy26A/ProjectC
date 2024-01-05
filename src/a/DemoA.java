3f4bcb22ad5b19f3bc22240c750816f2f891ac80
JAZUMA <JAZUMA@10.0.0.3>
2024-01-04 18:14:53 -0800

    initial commit

---
+++ src/a/DemoA.java
@@ -0,0 +1,10 @@
+package a;
+
+public class DemoA {
+	public static void main(String[]args) {
+		System.out.println("Monday");
+		
+		System.out.println("Batch 11");
+		System.out.println("Batch 11 Git Lecture");
+	}
+}
